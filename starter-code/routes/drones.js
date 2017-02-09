fdd9fd1b0e54681667ab668db638680c6e2de5e1
Suazolc <suazolc@gmail.com>
2017-02-09 17:16:16 +0100

    done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,12 +1,14 @@
+/*jshint esversion: 6*/
 const express = require('express');
 
 // require the Drone model here
 
 const router = express.Router();
-
+const drone = require('Drone');
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+
 });
 
 
