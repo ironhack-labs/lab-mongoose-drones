b5387c81dc2c986a0c318c22d25b4049e41a08d1
EnrikeGonzaloLegarra <enrikeglegarra@gmail.com>
2017-02-10 11:36:35 +0100

    not finished

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,33 @@
+/*jshint esversion:6*/
 // Iteration #1
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev');
+const Drone = require('../models/drone');
+
+const drone = [{
+    droneName: 'Drone1',
+    propellers: 2,
+    maxSpeed: 10,
+  },
+  {
+    droneName: 'Drone2',
+    propellers: 3,
+    maxSpeed: 20,
+  },
+  {
+    droneName: 'Drone3',
+    propellers: 4,
+    maxSpeed: 30,
+  }
+];
+
+
+Drone.create(drone, (err, docs) => {
+  if (err) {
+    throw err
+  };
+  docs.forEach((drone) => {
+    console.log(drone.droneName)
+  })
+  mongoose.connection.close();
+});
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,28 +1,56 @@
+/*jshint esversion:6*/
 const express = require('express');
 
 // require the Drone model here
-
+const Drone = require('../models/drone');
 const router = express.Router();
-
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+  Drone.find({}, function(err, drones) {
+    if (err) return next(err);
+    res.render('drones/index', {
+      title: 'Drones View',
+      drones
+    });
+  });
 });
 
-
-router.get('/drones/new', (req, res, next) => {
-  // Iteration #4
+router.get('/drones/:id', (req, res, next) => {
+  const id = req.params.id;
+  Drone.findOne({
+    _id: id
+  }, function(err, drone) {
+    if (err) return next(err);
+    res.render('drones/show', {
+      drone
+    });
+  });
 });
+
+/*router.get('/new', function(req, res, next) {
+  res.render('new', {
+    title: 'Add a new product'
+  });
+});
+
+router.post('/new', function(req, res, next) {
+  //const body = req.body;
+  const droneName = req.body.droneName;
+  const propellers = req.body.propellers;
+  const maxSpeed = req.body.maxSpeed;
+  const drone = new Drone(droneName, propellers, maxSpeed);
+  drone.save(function(err, doc) {
+    if (err) return next(err);
+    res.redirect('/drone');
+  });
+});
+*/
+
 
 router.post('/drones', (req, res, next) => {
   // Iteration #4
 });
-
-
-router.get('/drones/:id', (req, res, next) => {
-  // Iteration #3
-});
-
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #6 (Bonus)
