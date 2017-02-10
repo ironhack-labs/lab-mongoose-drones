6a3fd100f01652a1467b8db123a1af635f5eb8ff
Peter Fantasia <peter.vittorio@gmail.com>
2017-02-10 08:45:05 +0100

    3 exercises complete

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,23 @@
+/* jshint esversion: 6 */
+
 // Iteration #1
+
+const drones = [
+    { droneName: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { droneName: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { droneName: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+// bin/seeds.js
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev');
+const drone = require('../models/drone');
+
+// bin/seeds.js
+drone.create(drones, (err, docs) => {
+  if (err) { throw err };
+  docs.forEach( (drones) => {
+      console.log(drones);
+    });
+    mongoose.connection.close();
+  });
---
+++ starter-code/models/drone.js
@@ -0,0 +1,15 @@
+/* jshint  esversion:6 */
+
+const mongoose = require('mongoose');
+
+const Schema   = mongoose.Schema;
+
+const droneSchema = new Schema({
+  droneName       : String,
+  propellers : Number,
+  maxSpeed  : Number,
+});
+
+const drone = mongoose.model('drone', droneSchema);
+
+module.exports = drone;
