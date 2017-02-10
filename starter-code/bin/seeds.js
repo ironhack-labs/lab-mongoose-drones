cb2ab431e6b69238821516679a28b6969b96d386
ManuLintz <manuel.lintz@gmail.com>
2017-02-10 08:44:50 +0100

    done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,10 @@
 // Iteration #1
+const droneData = [
+  { droneName: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { droneName: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { droneName: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dronesPage');
+const Drone = require('../models/drone');
---
+++ starter-code/models/drone.js
@@ -0,0 +1,19 @@
+/* jshint esversion:6*/
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const DroneSchema = new Schema({
+  droneName: {
+    type: String
+    //required: true
+  },
+  propellers: {
+    type: Number
+    //required: true
+  },
+  maxSpeed: {
+    type: Number}}
+  );
+
+const Drone = mongoose.model('Drone', DroneSchema);
+module.exports = Drone;
