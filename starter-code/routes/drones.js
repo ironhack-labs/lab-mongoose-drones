9802298892edb2d42f6e07bc34290fde9e9be1d5
Adel Acebal <adelacebal@yahoo.com>
2017-04-26 17:31:11 -0400

    no style all functionality

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,41 @@
 const express = require('express');
 
-// require the Drone model here
+const Drone = require('../models/drone.js');
 
 const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2
+  Drone.find((err, droneList) => {
+    if(err){
+      next(err);
+      return;
+    }
+    res.render('drones/index.ejs', {
+    drones: droneList
+    });
+  });
 });
 
 
 router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+    res.render('drones/new.ejs');
 });
 
-router.post('/drones', (req, res, next) => {
-  // Iteration #3
+router.post('/drones/new', (req, res, next) => {
+  const theDrone = new Drone({
+    droneName: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  });
+  theDrone.save((err) => {
+    if(err){
+      next(err);
+      return;
+    }
+
+    res.redirect('/drones');
+  });
 });
 
 module.exports = router;
