8b1fea3d285054fb29b27572266b1286e2b8ef91
Gustavo Gutierrez <imguss@gmail.com>
2017-04-26 00:41:15 -0400

    finished and added some basic style to it

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,35 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const Drone = require('../models/drone');
+
+const drones = [
+  {
+    droneName: "Millenium Falcon",
+    propellers: 8,
+    maxSpeed: 100
+  },
+  {
+    droneName: "X Wing",
+    propellers: 4,
+    maxSpeed: 50
+  },
+  {
+    droneName: "Imperial Shuttle",
+    propellers: 3,
+    maxSpeed: 30
+  }
+];
+
+Drone.create(drones, (err, droneDoc) => {
+  if (err) {
+    throw err;
+  }
+
+  droneDoc.forEach( (oneDrone) => {
+    console.log(`Success: ${oneDrone.droneName} -> ${oneDrone._id}`);
+  });
+});
