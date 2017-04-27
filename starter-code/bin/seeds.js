73a67849fe7c4169e5b6ca013881b57a7f3e018c
edsonjmv <edsonjmv@gmail.com>
2017-04-27 16:03:16 +0200

    Done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,36 @@
 // Iteration #1
+/*jshint esversion:6*/
+
+const mongoose = require('mongoose');
+let Drone = require('../models/drone');
+
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const drones = [
+  {
+    droneName: 'Creeper XL 500',
+    propellers: 3,
+    maxSpeed: 12
+  },
+  {
+    droneName: 'Racer 57',
+    propellers: 4,
+    maxSpeed: 20
+  },
+  {
+    droneName: 'Courier 3000i',
+    propellers: 6,
+    maxSpeed: 18 }
+];
+
+Drone.collection.drop();
+
+Drone.create(drones, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+  docs.forEach((drone) => {
+    console.log(drone.droneName);
+  });
+  mongoose.connection.close();
+});
