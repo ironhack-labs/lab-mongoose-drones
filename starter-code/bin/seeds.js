619008a942c0ad8cce7d14e5eef2445999830ab4
Miguel <maldesan@gmail.com>
2017-04-26 21:02:21 -0400

    done with iterations. just working on CSS

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,35 @@
 // Iteration #1
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/lab-drones');
+
+const Drone=require('../models/drone.js');
+
+const drones = [
+  {
+    droneName:'Inspire 2',
+    propellers:4,
+    maxSpeed :67
+  },
+  {
+    droneName:'XFold Dragon X12',
+    propellers:12,
+    maxSpeed: 75
+  },
+  {
+    droneName:'Gemo Copter',
+    propellers:4,
+    maxSpeed: 117
+  }
+];
+
+Drone.create(drones,(err,docs) => {
+  if(err){
+    console.log('Dang! Error when running the seed file.')
+    return;
+  }
+
+  docs.forEach((oneDoc)=>{
+    console.log(`${oneDoc.droneName} -> ${oneDoc._id}`);
+  });
+  mongoose.disconnect();
+});
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,41 @@
 const express = require('express');
 
 // require the Drone model here
-
+const Drone=require('../models/drone.js');
 const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+  Drone.find((err,drones)=>{
+    if(err){
+      return;
+    }
+    res.render('drones/index.ejs',{
+      drones : drones
+    });
+  });
 });
 
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
+  res.render('drones/new.ejs');
 });
 
-router.post('/drones', (req, res, next) => {
+router.post('/drones/new', (req, res, next) => {
   // Iteration #3
+  const newDrone = new Drone({
+    droneName: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  });
+  newDrone.save(err=>{
+    if(err){
+      return;
+    }
+    res.redirect('/drones');
+  });
 });
 
 module.exports = router;
