347718526f835cc6d4b65dec0a3245fad39a90d3
SatishS <satish25@gmail.com>
2017-04-27 18:25:30 -0400

    Got up to Iteration 2

---
+++ starter-code/models/drone.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const droneSchema = new Schema( {
+  droneName: {type: String},
+  propellers: { type: Number},
+  maxSpeed: {type: Number}
+});
+
+const Drone = mongoose.model('Drone', droneSchema);
+
+module.exports = Drone;
