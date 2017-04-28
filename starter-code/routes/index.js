b2bde472b1233e022a9775254f6640a04248837e
TuckerDS <jjperez.tucker.dev@gmail.com>
2017-04-28 15:03:42 +0200

    MAD Joanna - Tucker

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,13 @@
+/*jshint esversion: 6*/
+
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Drnz' });
+  res.render('index', { title: 'Drones' });
 });
+
 
 module.exports = router;
