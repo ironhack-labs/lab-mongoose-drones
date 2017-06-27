616e8bdbc36dc20839bbc901fd36ceea80a0a317
Deokjae Jeon <deokjj@github.com>
2017-06-27 18:38:04 -0400

    done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,47 @@
 const express = require('express');
-
 // require the Drone model here
+const DroneModel = require('../models/droneModel.js');
 
 const router = express.Router();
 
 
+//List of drones
 router.get('/drones', (req, res, next) => {
-  // Iteration #2
+  // allDrones is an array of drones in the collection
+  DroneModel.find((err, allDrones) => {
+    if(err){
+      next(err);
+      return;
+    }
+    res.render('drones/index.ejs',{allDrones:allDrones});
+
+  });
 });
 
 
+//Adding 'form'
 router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+  res.render('drones/new.ejs');
 });
 
+//When form added-
 router.post('/drones', (req, res, next) => {
-  // Iteration #3
+
+  //with DroneModel create new drone object
+  //by parsing body "name" of input
+  const newDrone = new DroneModel({
+    droneName: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  });
+  newDrone.save((err)=>{
+    if(err){
+      next(err);
+      return;
+    }
+    //redirecting to list
+    res.redirect('/drones');
+  });
 });
 
 module.exports = router;
