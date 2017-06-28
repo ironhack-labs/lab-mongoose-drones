c946240cca9b07f51f42a04cc47bfe20a9b32e01
Darren Whyte <darrenjevon@gmail.com>
2017-06-27 21:08:48 -0400

    done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,52 @@
 const express = require('express');
 
 // require the Drone model here
+const Drone = require('../models/drone.js');
 
 const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+  Drone.find((err, droneResults) =>{
+    if (err){
+      next(err);
+      return;
+    }
+    res.render('drones/drone-list-view.ejs', {dronesAndShit: droneResults});
+  });
 });
+
 
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
+  res.render("drones/new-drone-view.ejs");
 });
+
+
+
+
+
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
+  const theDrone = new Drone({
+    droneName: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  });
+
+  theDrone.save((err) =>{
+    if(err){
+      // use next() to skip to the ERROR PAGE
+      next(err);
+      return;
+    }
+
+    res.redirect('/drones');
+        // you can ONLY redirect to a URL 🌍
+  });
 });
 
 module.exports = router;
