81cfc2b851fb249f9347d9165d718591086edd88
mikelRumayor <mikel.rumayor@gmail.com>
2017-07-06 17:39:23 +0200

    iteration 3 done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -7,10 +7,10 @@ const router = express.Router();
 // CRUD => R: Retrieve All
 router.get('/drones', (req, res, next) => {
   // Iteration #2
-  Drone.find({}, (err, p) => {
+  Drone.find({}, (err, drone) => {
     res.render('drones/index', {
       title: 'DRONES',
-      drones: p
+      drones: drone
     });
   });
 });
@@ -18,10 +18,21 @@ router.get('/drones', (req, res, next) => {
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
+  res.render('drones/new', {
+    title: 'DRONES'
+  });
 });
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
+  let drone = new Drone({
+    droneName  : req.body.droneName,
+    propellers : req.body.propellers,
+    maxSpeed   : req.body.maxSpeed
+  });
+  drone.save((err, obj) => {
+    res.redirect('/drones');
+  });
 });
 
 module.exports = router;
