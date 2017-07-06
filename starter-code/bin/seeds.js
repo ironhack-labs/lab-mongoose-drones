745e1319bfdc70eb8bdae179802363fa80b3f3bd
mikelRumayor <mikel.rumayor@gmail.com>
2017-07-06 16:07:04 +0200

    iteration 1 done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,36 @@
 // Iteration #1
+const Drone = require('../models/Drone');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/dronesDB')
+    .then(() => {
+        const droneData = [{
+            droneName: 'Creeper XL 500',
+            propellers: 3,
+            maxSpeed: 12
+        }, {
+            droneName: 'Racer 57',
+            propellers: 4,
+            maxSpeed: 20
+        }, {
+            droneName: 'Courier 3000i',
+            propellers: 6,
+            maxSpeed: 18
+        }]
+
+        let dronesObj = droneData.map(p => new Drone(p));
+
+        let promises = dronesObj.map(p => new Promise((resolve, reject) => {
+            p.save((err, obj) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(`New drones created [${obj.droneName}] with ID:${obj._id}`);
+                    resolve(true)
+                }
+            })
+        }));
+        Promise.all(promises).then(() => {
+            mongoose.connection.close()
+        })
+    });
