c733f1ee0d37fc2a49e2237f56c3339e95bc5434
rosadl <rosadl84@gmail.com>
2017-07-06 16:43:58 +0200

    update

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,23 @@
 // Iteration #1
+const Drone = require('../models/drone');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev')
+  .then(() => {
+    let drones = [
+      { droneName: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+      { droneName: 'Racer 57', propellers: 4, maxSpeed: 20 },
+      { droneName: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+    ];
+
+    let droneObj = drones.map( d => new Drone(d));
+
+    droneObj.forEach( d => d.save( (err, obj) =>{
+      if(err){
+        console.log(err);
+      }else{
+        console.log(`New drone created [${obj.droneName}] with ID:${obj._id}`);
+      }
+    }));
+
+    //mongoose.connection.close();
+  });
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,12 +1,26 @@
 const express = require('express');
-
+const Drone = require('../models/drone');
 // require the Drone model here
+
+
 
 const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+    Drone.find({}, (err, d) => {
+      if(err){
+  return next;
+}
+      else{
+console.log(d);
+        res.render('drones/index', {
+        drones: d
+
+      });
+    }
+    });
 });
 
 
