11ade4bfb64e34111379c6ee4a53d6a43b8e7f42
Chilliano <cilllian.murchu@gmaill.com>
2017-07-06 16:27:18 +0200

    still needs styling

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -16,12 +16,31 @@ router.get('/', (req, res, next) => {
 });
 
 
-router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+router.get('/new', (req, res, next) => {
+  res.render("drones/new")
 });
 
-router.post('/drones', (req, res, next) => {
-  // Iteration #3
+router.post('/', (req, res, next) => {
+
+  const droneInfo = {
+
+    droneName: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+
+
+  };
+
+  const newDrone = new Drone(droneInfo);
+  newDrone.save(error =>{
+    if (error) {
+      next(error);
+      return; 
+    } else {
+      res.redirect("/drones");
+    }
+  })
+  
 });
 
 module.exports = router;
