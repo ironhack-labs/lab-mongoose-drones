c0f597042660c04580edd4f074574266260709cd
Paty Gilbon <anapatygilbon@gmail.com>
2017-07-06 15:49:23 +0200

    done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,19 @@
 // Iteration #1
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const drones = [
+  { droneName: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { droneName: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { droneName: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+const Drone=require('../models/drone');
+Drone.create(drones,(err, docs)=>{
+  if(err){
+    throw err;
+  }
+  docs.forEach((drones)=>{
+    console.log(drones.droneName);
+  });
+  mongoose.connection.close();
+});
