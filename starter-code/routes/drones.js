6c4c00122f1ba3a8d92deaa9636c01b1d3b84d3d
sara <sara-cg@github.com>
2017-07-06 16:40:33 +0200

    almost finished

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,18 +1,39 @@
 const express = require('express');
 
 // require the Drone model here
-
+const Drone = require('../models/drone.js');
 const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+  Drone.find({}, (err, drones) => {
+    if (err) {
+      return next(err);
+    } else {
+    res.render('drones/index', {
+      drones: drones
+    });
+  }
+});
 });
 
-
-router.get('/drones/new', (req, res, next) => {
+router.post('/drones/new', (req, res, next) => {
   // Iteration #3
+  let droneObj = new Drone({
+    droneName: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  });
+  droneObj.save((err, obj) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.redirect('/drones');
+    }
+  });
 });
+
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
