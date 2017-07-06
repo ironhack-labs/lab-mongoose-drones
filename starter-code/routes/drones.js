b817b11ada65faaabd716a6c32a058ecdcd30c37
Jorge Pedrejon <jgpedrejon@gmail.com>
2017-07-06 20:32:35 +0200

    8:30

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,48 @@
 const express = require('express');
 
-// require the Drone model here
+const Drones = require('../models/Drone');
 
 const router = express.Router();
 
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2
+router.get('/', (req, res, next) => {
+    Drones.find({}, (err, dron) => {
+      if(err){
+      console.log("ERROR");
+      console.log(err);
+      } else {
+          res.render('drones/index', {dron:dron});
+      }
+
+    });
+
 });
 
 
-router.get('/drones/new', (req, res, next) => {
+router.get('/new', (req, res, next) => {
   // Iteration #3
+  console.log("Estoy en NEW, acabo de entrar");
+  res.render('drones/new');
+
 });
 
-router.post('/drones', (req, res, next) => {
+router.post('/', (req, res, next) => {
   // Iteration #3
+  const newDroneInfo = {
+     droneName: req.body.droneName,
+     propellers: req.body.propellers,
+     maxSpeed: req.body.maxSpeed
+ };
+
+  const newDrone = new Drone(newDroneInfo);
+
+  newDrone.save( (err) => {
+  if (err) {
+    return render('drones/new', { errors: newProduct.errors });
+  }
+  return res.redirect('/index');
+});
+
 });
 
 module.exports = router;
