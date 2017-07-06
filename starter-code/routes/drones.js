130ea19cf0d2552a508ac5e0ce84099877cf00f6
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-07-06 16:36:56 +0200

    second iteration

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -7,6 +7,17 @@ const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+  Drone.find({}, (err, p) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', {
+        title: 'Drone shop',
+        drones: p
+      });
+    }
+  }
+);
 });
 
 
