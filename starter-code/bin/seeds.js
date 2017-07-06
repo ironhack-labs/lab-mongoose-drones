190b48870369e7e288daa942c4b1c1038dfbc1f2
Petra Spirkova <pspirkova@gmail.com>
2017-07-06 14:58:07 +0200

    iteration 1 done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,23 @@
-// Iteration #1
+const Drone = require('../models/drone');
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const drones = [
+{name: 'Hawk', propellers: 3, maxSpeed: 12},
+{name: 'Eagle', propellers: 4, maxSpeed: 20},
+{name: 'Sentinel', propellers: 6, maxSpeed: 18}
+];
+
+Drone.create(drones, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+
+  docs.forEach((drone) => {
+    console.log(drone._id)
+  });
+    mongoose.connection.close();
+});
+
+
