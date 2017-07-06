c2ffe4911be6ee9e44301c238c4ce72284b03c32
Ana Milojevic <anamilojevicd@gmail.com>
2017-07-06 16:37:24 +0200

    wip

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,26 @@
 // Iteration #1
+const Drone = require('../models/Drone');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/drones-dev')
+  .then(() => {
+    let drones = [
+      { droneName: 'Dronify', propellers: 1, maxSpeed: 18 },
+      { droneName: 'Super Comet Dron', propellers: 4, maxSpeed: 27 },
+      { droneName: 'Master Cool Pro Droning', propellers: 2, maxSpeed: 21 },
+    ];
+
+    let droneObj = drones.map( e => new Drone(e));
+
+    droneObj.forEach( e => e.save( (err, obj) =>{
+      if(err){
+        console.log(err);
+      }else{
+        console.log(`New drone created [${obj.droneName}] with ID:${obj._id}`);
+        // if(drones.length - 1 == e){
+        //   console.log("ended connection");
+        //   mongoose.connection.close();
+        // }
+      }
+    }));
+  });
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,12 +1,21 @@
 const express = require('express');
-
 // require the Drone model here
+const Drones = require('../models/Drone.js');
 
 const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+  Drones.find({}, (err, e) => {
+    if(err) {
+      return next(err);
+    } else {
+      res.render('index', {
+        title: 'Drones Market'
+      });
+    }
+  });
 });
 
 
