83e1ec2274ddf05b27fcf5fe536a9caf8fe50d24
Petra Spirkova <pspirkova@gmail.com>
2017-07-06 15:36:55 +0200

    2nd iteration

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/drones-dev');
 
 const drones = [
-{name: 'Hawk', propellers: 3, maxSpeed: 12},
-{name: 'Eagle', propellers: 4, maxSpeed: 20},
-{name: 'Sentinel', propellers: 6, maxSpeed: 18}
+{droneName: 'Hawk', propellers: 3, maxSpeed: 12},
+{droneName: 'Eagle', propellers: 4, maxSpeed: 20},
+{droneName: 'Sentinel', propellers: 6, maxSpeed: 18}
 ];
 
 Drone.create(drones, (err, docs) => {
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
-// require the Drone model here
+const Drone = require('../models/drone')
 
 const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2
+  Drone.find({}, (err, drones) => {
+    if (err) {
+      next(err)
+      return
+    } 
+  res.render('drones/index', { drones })
+  })
 });
 
 
