a69aaf5bbee7dc0fb5e5342e7cf2856406ffcec4
Jorge Avila <javila83@gmail.com>
2017-07-06 15:48:55 +0200

    done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,32 @@
-// Iteration #1
+const mongoose = require('mongoose');
+mongoose.connect ('mongodb://localhost:27017/drones-dev');
+const Drone = require('../models/drone');  // will obtain the exported variable (if any)
+
+const droneData = [
+  {
+    droneName: 'Creeper XL 500',
+    propellers: 3,
+    maxSpeed: 12,
+  },
+  {
+    droneName: 'Racer 57',
+    propellers: 4,
+    maxSpeed: 20,
+  },
+  {
+    droneName: 'Courier 3000i',
+    propellers: 6,
+    maxSpeed: 18,
+  }
+];
+
+Drone.create(droneData,(err,docs) => {   // we convert the array of products and save them to the database
+  if (err) {
+    throw err;
+  }
+
+  docs.forEach((drone) => {
+    console.log(drone.droneName);
+  });
+  mongoose.connection.close();  // we close (kill) the connection to the server
+});
