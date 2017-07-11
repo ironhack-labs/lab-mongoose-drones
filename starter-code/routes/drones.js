8a16af26262b6ae8030ed08c812ec00a0db803f4
Llucgs23 <lluc.guarro.serrahima@gmail.com>
2017-07-11 21:03:02 +0200

    done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,40 @@
+
 const express = require('express');
-
-// require the Drone model here
-
 const router = express.Router();
+const Drone = require('../models/drone');
 
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2
+  Drone.find({}, (err, drones) => {
+    if (err){
+      next(err);
+    } else {
+      console.log(drones);
+      res.render("drones/index", {drones})
+    }
+  })
 });
 
 
 router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+  res.render("drones/new");
 });
 
 router.post('/drones', (req, res, next) => {
-  // Iteration #3
+  const dronesData = {
+    droneName : req.body.droneName,
+    propellers : req.body.propellers,
+    maxSpeed : req.body.maxSpeed,
+  }
+
+const newDrone = new Drone(dronesData);
+newDrone.save( (err) => {
+    if(err) {
+      return next(err)
+    } else {
+      res.redirect('/drones');
+    }
+  })
 });
 
 module.exports = router;
