ac2e5abad9d51a78b3a33089cfea4d333ef1150f
Marc Nicolau <info@marcnicolau.net>
2017-07-11 20:13:04 +0200

    done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,43 @@
 const express = require('express');
-
-// require the Drone model here
-
 const router = express.Router();
+
+const Drone = require('../models/drone');
 
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2
+  Drone.find({}, (err, drone) => {
+        if (err) {
+            next(err);
+        } else {
+            console.log(drone);
+            res.render('drones/index', {
+                drone
+            });
+        }
+    });
 });
 
 
 router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+  res.render('drones/new');
 });
 
 router.post('/drones', (req, res, next) => {
-  // Iteration #3
+  const droneInfo = {
+        droneName: req.body.droneName,
+        propellers: req.body.propellers,
+        maxSpeed: req.body.maxSpeed,
+    };
+
+    const newDrone = new Drone(droneInfo);
+
+    newDrone.save((err) => {
+        if (err) {
+            return next(err)
+        } else {
+            res.redirect('/drones')
+        }
+    });
 });
 
 module.exports = router;
