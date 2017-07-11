08eab9af8788d47caa4bb8f71c22af66d76d3279
Jordi Piella <jordipiella@gmail.com>
2017-07-11 13:54:12 +0200

    Done

---
+++ starter-code/models/drone.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const droneSchema = new Schema ({
+    droneName  : String,
+    propellers : Number,
+    maxSpeed   : Number
+});
+
+const Drone = mongoose.model('Drone', droneSchema);
+
+module.exports = Drone;
