852a294e3a8d3473bc4e521fb0156e30fb39f779
Dharma Velasco <dharma.giselle@gmail.com>
2017-09-04 12:12:25 -0400

    drone project

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,30 @@
 // Iteration #1
+const mongoose = require('mongoose');
+
+const DroneModel = require('../models/drone.js');
+
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const droneData = [
+  { droneName: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { droneName: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { droneName: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+DroneModel.create(
+  droneData,
+
+  //2nd argument -> callback
+  (err, dronesAfterSave) => {
+    if(err) {
+      console.log('Create error 😛');
+      return;
+    }
+
+    dronesAfterSave.forEach((droneProduct) => {
+      console.log('Product ------>' + droneProduct.droneName);
+    });
+
+  }
+
+);
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const DroneModel = require('../models/drone.js');
+
 // require the Drone model here
 
 const router = express.Router();
@@ -7,15 +9,47 @@ const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+  DroneModel.find((err, allDrones) => {
+    if(err) {
+      next(err);
+      return;
+    }
+
+    res.locals.drones = allDrones;
+
+    res.render('drones/index.ejs');
+
+  });
 });
 
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
+  res.render('drones/new.ejs');
 });
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
+  const theDrones = new DroneModel({
+    droneName: req.body.droneName,
+    propellers: req.body.dronePropellers,
+    maxSpeed: req.body.droneMaxSpeed
+  });
+
+  theDrones.save((err) => {
+    if (err) {
+      //skip to the error handlr middleware
+      next(err);
+      return;
+    }
+
+      // STEP #3 redirect
+      //ALWAYS redirect after a succesful POST to avoid resubmitting
+      res.redirect('/drones');
+      // you can only redirect to a URL
+  });
+
+
 });
 
 module.exports = router;
