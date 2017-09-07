1c0b025ab253f433e432ddfb676092b4c1c1b2da
Frances Morales Velilla <frances0688@gmail.com>
2017-09-07 15:56:11 +0200

    working on iteration 2

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,12 +1,17 @@
 const express = require('express');
-
+const Drone = require('../models/drone');
 // require the Drone model here
 
 const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
-  // Iteration #2
+  Drone.find({}, (err, drones) => {
+    if(err) { next(err);} 
+    res.render('drone', { 
+      drones
+    });
+	});
 });
 
 
