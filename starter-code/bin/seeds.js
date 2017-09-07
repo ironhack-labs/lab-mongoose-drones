93a03be5bf656ec68d80e317a1e18f81f4069ad0
Luisa Fernanda Salazar Hoyos <luisafernandasalazarhoyos@MacBook-Pro-de-Luisa.local>
2017-09-07 17:47:37 +0200

    queda por terminar iteracion2

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -4,7 +4,12 @@ const {dbURL} = require('../config/db');
 
 mongoose.connect(dbURL, {useMongoClient: true});
 
-const drone = [{droneName: "Santi", propellers: 4, maxSpeed: 18}];
+const drone = [
+  {droneName: "Santi", propellers: 4, maxSpeed: 18},
+  { droneName: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { droneName: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { droneName: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
 
 Drone.create(drone, (err, docs) => {
   if(err) {
