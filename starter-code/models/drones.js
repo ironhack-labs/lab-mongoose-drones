a8543e36070b360884aaeeae41a3bedcd5e015c7
Philip Wisner <philipwisner@gmail.com>
2017-09-07 17:11:09 +0200

    added requires

--- starter-code/models/drones.js
+++ starter-code/models/drones.js
@@ -2,9 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DroneSchema = new Schema ({
-  droneName : {type: String},
-  propellers: {type: Number},
-  maxSpeed: {type: Number},
+  droneName : {type: String,
+               required: [true, 'Please enter the name of the Drone']
+              },
+  propellers: {type: Number,
+               required: [true, 'Please enter the number of propellers'],
+               min: 0
+              },
+  maxSpeed: {type: Number,
+             required: [true, 'Please enter the maxSpeed'],
+             min: 0
+            },
 })
 
 const Drone = mongoose.model('Drone', DroneSchema);
