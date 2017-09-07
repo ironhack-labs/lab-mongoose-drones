e18900242f3b53ce015951f7cc29005c351e08d0
Philip Wisner <philipwisner@gmail.com>
2017-09-07 14:43:33 +0200

    added favicon

--- starter-code/app.js
+++ starter-code/app.js
@@ -22,8 +22,7 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.locals.title = 'Drnz';
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'drone.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
