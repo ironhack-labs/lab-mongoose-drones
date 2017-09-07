16673c428fdc4238ed8328d8d35144eb76dee10d
Manuel del Pino Lucena <manueldelpinolucena@gmail.com>
2017-09-07 17:04:12 +0200

    Iteration 3 done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -19,10 +19,24 @@ router.get('/drones', (req, res, next) => {
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
+  res.render('drones/new',{title:'Add a drone'});
 });
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
+  const droneInfo = {
+      droneName: req.body.droneName,
+      propellers: req.body.propellers,
+      maxSpeed: req.body.maxSpeed
+  };
+
+  // Create a new Drone with the params
+  const newDrone = new Drone(droneInfo);
+  newDrone.save( (err) => {
+    if (err) { return next(err); }
+    // redirect to the list of drones if it saves
+    return res.redirect('/drones');
+  });
 });
 
 module.exports = router;
