4e85d7d2926f5f9516db5331cbb2c56f6290568d
ritorrehuelamo <torrehuelamo@gmail.com>
2017-09-07 16:17:04 +0200

    Iteration 1

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,33 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone')
+const {dbURL} = require('../config/db.js')
+
+mongoose.connect(dbURL, {useMongoClient: true})
+
+const drones = [
+	{
+		droneName: 'Drone 1',
+		propellers: '10',
+		maxSpeed: '100'
+	},
+	{
+		droneName: 'Drone 2',
+		propellers: '10',
+		maxSpeed: '100'
+	},
+	{
+		droneName: 'Drone 3',
+		propellers: '10',
+		maxSpeed: '100'
+	}
+]
+
+Drone.create(drones, (err, docs) => {
+	if (err) {
+		throw err
+	}
+	docs.forEach(d => d.droneName)
+	mongoose.connection.close()
+})
