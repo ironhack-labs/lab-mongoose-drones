1adb5cd2e1efc1b44af99d277e46e21be1063118
Luisa Fernanda Salazar Hoyos <luisafernandasalazarhoyos@MacBook-Pro-de-Luisa.local>
2017-09-07 16:45:24 +0200

    hecha iteracion 1

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,15 @@
-// Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone');
+const {dbURL} = require('../config/db');
+
+mongoose.connect(dbURL, {useMongoClient: true});
+
+const drone = [{droneName: "Santi", propellers: 4, maxSpeed: 18}];
+
+Drone.create(drone, (err, docs) => {
+  if(err) {
+    throw err;
+  }
+  docs.forEach(d => console.log(d));
+  mongoose.connection.close();
+});
