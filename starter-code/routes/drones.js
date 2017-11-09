bc93e43265aacf1befb2c5c8dd7e5a6a8e1aa3d6
Juan Cuadra <jcb8793@gmail.com>
2017-11-09 15:53:39 +0100

    Done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,48 @@
-const express = require('express');
+const express = require("express");
 
 // require the Drone model here
-
+var Drone = require("../models/drone").Drone;
 const router = express.Router();
 
-
-router.get('/drones', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // Iteration #2
+  Drone.find({}, (err, result) => {
+    if (err) {
+      console.log("Error Monongo: ", err);
+    } else {
+      const data = {
+        drones: result
+      };
+      res.render("drones", data);
+    }
+  });
 });
 
-
-router.get('/drones/new', (req, res, next) => {
+router.get("/new", (req, res, next) => {
   // Iteration #3
+  Drone.find({}, (err, result) => {
+    if (err) {
+      console.log("Error Monongo: ", err);
+    } else {
+      res.render("drones/new");
+    }
+  });
 });
 
-router.post('/drones', (req, res, next) => {
+router.post("/", (req, res, next) => {
   // Iteration #3
+  const droneInfo = {
+    droneName: req.body.droneName,
+    propellers: req.body.proppelers,
+    maxSpeed: req.body.maxSpeed
+  };
+  const newDrone = new Drone(droneInfo);
+  newDrone.save(err => {
+    if (err) {
+      return next(err);
+    }
+    return res.redirect("/drones");
+  });
 });
 
 module.exports = router;
