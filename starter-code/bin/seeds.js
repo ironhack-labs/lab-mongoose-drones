46d3d0f0bd01b713a0738ced73755bd017a5628d
Juan Vallejo-Nagera <juanvallejo-nagera@Juans-MacBook-Pro.local>
2017-11-09 13:57:29 +0100

    iteration 3

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,28 @@
-// Iteration #1
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drone-lab', {useMongoClient: true});
+const Drone = require('../models/drone');
+
+const sebastian = [
+  {
+    droneName: "Yurema",
+    propellers: 5,
+    maxSpeed: 100,
+  },
+  {
+    droneName: "Juan",
+    propellers: 4,
+    maxSpeed: 200,
+  },
+  {
+    droneName: "Victor",
+    propellers: 3,
+    maxSpeed: 300,
+  }
+];
+
+Drone.create(sebastian, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+  mongoose.connection.close();
+});
