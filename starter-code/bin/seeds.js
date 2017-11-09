bee2410843a216055044cbff147d80779d3c6457
ventdlnord <ventdlnord@gmail.com>
2017-11-09 16:01:47 +0100

    first iteration

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,37 @@
+'use strict';
 // Iteration #1
+
+
+const Drone = require("../models/drone").Drone;
+
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/drones');
+
+const droneData = [{
+          droneName: "R2D2",
+          propellers: 4,
+          maxSpeed: 20,
+     },
+     {
+          droneName: "C3PO",
+          propellers: 8,
+          maxSpeed: 25,
+     },
+     {
+          droneName: "BB8",
+          propellers: 4,
+          maxSpeed: 15,
+     },
+];
+
+Drone.create(droneData, (err, res) => {
+     if (err) {
+          console.log(err);
+     } else {
+          console.log(res);
+     }
+});
+
+
+//mongoose.connect.close();
