c78595fafbeb7fedb1561fb3a6eed602de5ab848
Alberto Rivera <alberto.rivera.merida@gmail.com>
2017-11-09 12:44:41 +0100

    Terminado sin CSS

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,39 @@
+// bin/seeds.js
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev', {useMongoClient: true});
+const Drone = require('../models/drones');
 // Iteration #1
+
+
+
+const drones = [
+  {
+    droneName       : 'Cristian',
+    propellers      : 5,
+    maxSpeed   : 10,
+
+  },
+  {
+    droneName       : 'Alberto',
+    propellers      : 5,
+    maxSpeed   : 10,
+  },
+  {
+    droneName       : 'Thor',
+    propellers      : 15,
+    maxSpeed   : 50,
+  }
+];
+//Borrar antes de crear-->Es lo que deberíamos hacer.
+Drone.collection.drop();
+// bin/seeds.js
+Drone.create(drones, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+
+  docs.forEach((drone) => {
+    console.log(drone.droneName)
+  });
+  mongoose.connection.close();
+});
