0ebf6ab253b55722458988ae8bd86d04d8d33d70
Dario Vigil <dariovigil@gmail.com>
2017-11-09 13:15:22 +0100

    Done.

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -20,10 +20,22 @@ router.get('/drones', (req, res, next) => {
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
+  res.render('drones/new');
 });
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
+  const droneInfo = {
+    droneName : req.body.droneName,
+    propellers : req.body.propellers,
+    maxSpeed : req.body.maxSpeed
+  };
+  const newDrone = new Drone(droneInfo);
+
+  newDrone.save((err) => {
+    if(err) { return next(err); }
+    return res.redirect('/drones');
+  });
 });
 
 module.exports = router;
