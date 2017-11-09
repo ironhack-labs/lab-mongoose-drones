ae00b1909836b23e9144f9f6d3acdb7713562999
Dominik Lasch <dl@exutec.de>
2017-11-09 16:07:16 +0100

    done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,23 @@
 // Iteration #1
+
+const Drone = require("../models/drones").Drone;
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/drones-dev");
+
+const droneData = [
+  { droneName: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { droneName: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { droneName: "Courier 3000i", propellers: 6, maxSpeed: 18 },
+  { droneName: "Cristian", propellers: 8, maxSpeed: 99 }
+];
+
+droneData.forEach((el, ix) => {
+  Drone.create(el, (err, drone) => {
+    if (err) {
+      return handleError(err);
+    }
+  });
+});
+
+mongoose.disconnect();
