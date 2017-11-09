a8865b42defdf7a6e852a556a3f19fae9d7fabbd
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-09 12:50:40 +0100

    iteration 1

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/shop-drones', {useMongoClient: true});
+const Dron = require('../models/drone.js');
+
+droneProducts = [{ droneName: "DJI Phantom 3", propellers: 4, maxSpeed: 20},
+                 { droneName: "DJI Phantom 4", propellers: 5, maxSpeed: 50},
+                 { droneName: "DJI Phantom 5", propellers: 6, maxSpeed: 70}];
+
+Dron.collection.drop();
+// bin/seeds.js
+Dron.create(droneProducts, (err, drones) => {
+ if (err) {throw err;}
+
+ drones.forEach((product) => {
+   console.log(product.droneName);
+ });
+
+ mongoose.connection.close();
+
+});
