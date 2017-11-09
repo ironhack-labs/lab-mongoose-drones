0c67dcc1293105ef0fd8930ee290f90ed333d31c
Nikolaas Verlee <nikotomad@gmail.com>
2017-11-09 12:39:19 +0100

    Iteration 1 done :)

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,38 @@
-// Iteration #1
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones', {useMongoClient: true});
+
+const Drone = require('../models/drone');
+
+const drones = [
+  {
+    droneName: 'Irondrone 2K',
+    propellers: 4,
+    maxSpeed: 700,
+  },
+  {
+    droneName: 'Xiaomi Phantom',
+    propellers: 6,
+    maxSpeed: 1500,
+  },
+  {
+    droneName: 'JRC',
+    propellers: 4,
+    maxSpeed: 100,
+  }
+];
+
+Drone.collection.drop();
+
+Drone.create(drones, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+
+  docs.forEach((drone) => {
+    console.log(drone.droneName)
+    console.log(drone.propellers)
+    console.log(drone.maxSpeed)
+    console.log('------------')
+  });
+  mongoose.connection.close();
+});
