f49fa1a41e913a00777c6b1ca3d640311d32ab99
Abraham Castro <cucharadepalo@gmail.com>
2017-11-09 12:28:47 +0100

    Iteration 1

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,31 @@
 // Iteration #1
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev', {useMongoClient: true});
+const Drones = require('../models/drones');
+
+const droneData = [
+  {
+  droneName: 'Creeper XL 500',
+  propellers: 3,
+  maxSpeed: 12
+}, {
+  droneName: 'Racer 57',
+  propellers: 4,
+  maxSpeed: 20
+}, {
+  droneName: 'Courier 3000i',
+  propellers: 6,
+  maxSpeed: 18
+}
+];
+
+Drones.create(droneData, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+
+  docs.forEach((drone) => {
+    console.log(drone.droneName)
+  });
+  mongoose.connection.close();
+});
