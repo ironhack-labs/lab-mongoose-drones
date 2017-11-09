c212a9b4c2ed507de394e65328fa00abc837aa8a
3lv27 <elvin.gm27@gmail.com>
2017-11-09 16:02:27 +0100

    iteration 2 done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,23 @@
 // Iteration #1
+
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone').Drone;
+
+
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const droneData = [
+    { droneName: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { droneName: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { droneName: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+Drone.create(droneData,(err, doc) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('Yeah! a drone has been created!')
+    }
+    mongoose.disconnect();
+
+} );
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,12 +1,25 @@
 const express = require('express');
 
 // require the Drone model here
+const Drone = require('../models/Drone').Drone;
 
 const router = express.Router();
 
 
-router.get('/drones', (req, res, next) => {
+router.get('/', (req, res, next) => {
   // Iteration #2
+  Drone.find({}, (err, drones) => {
+    if (err) {
+      next(err);
+    } else {
+      // console.log(res);
+      const data = {
+        drones: drones
+      }
+      console.log(data)
+      res.render('drones/index', data);
+    }
+  });
 });
 
 
