385da38e3448e04be88eb5a2676011255fd582b1
Anyelina Moreno Robledo <anyelinamoreno@gmail.com>
2017-11-09 13:58:10 +0100

    Progress Iteration 2 and 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -31,9 +31,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+
 app.use('/', drones);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -7,7 +7,10 @@ const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
-  
+  Drone.find({},(err, drones))=> {
+
+  return next(err) : err ? res.render("drones/index")
+  }
 });
 
 
@@ -16,6 +19,11 @@ router.get('/drones/new', (req, res, next) => {
 });
 
 router.post('/drones', (req, res, next) => {
+  const dronesInfo = {
+    droneName: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed : req.body.maxSpeed
+  }
   // Iteration #3
 });
 
