e718f61745af76a5cab7384222998264ff4f1415
Ernesmh <ernesmh@gmail.com>
2017-11-09 12:15:50 +0100

    initial

--- starter-code/app.js
+++ starter-code/app.js
@@ -22,6 +22,7 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.locals.title = 'Drnz';
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
