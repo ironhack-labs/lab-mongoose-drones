c3f4e38f2c4a32f6cc08a1533fbc05cbf5f32630
Sarah Fontes <sarahfontes@Sarahs-MacBook-Pro.local>
2017-11-09 16:06:32 +0100

    until second iteration

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,32 @@
+const mongoose = require("mongoose");
+const Drone = require("../models/drone").Drone;
+
+mongoose.connect("mongodb://localhost/dronedb");
+
 // Iteration #1
+const droneData = 
+ [{
+  droneName: "firstDrone", 
+  propellers: 100, 
+  maxSpeed: 100
+},
+{
+  droneName: "secondDrone", 
+  propellers: 200, 
+  maxSpeed: 200
+},
+{
+  droneName: "thirdDrone", 
+  propellers: 300, 
+  maxSpeed: 300
+}
+ ];
+
+Drone.create(droneData, function(err, docs){
+  if(err){
+    console.log(err);
+  } else {
+    console.log('Drone created!');
+  }
+  mongoose.disconnect();
+});
