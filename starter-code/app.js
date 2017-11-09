07a3a72778a097db381969629240a254d8df77a0
Nikolaas Verlee <nikotomad@gmail.com>
2017-11-09 13:43:36 +0100

    Iteration 3 done! :)

--- starter-code/app.js
+++ starter-code/app.js
@@ -23,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(expressLayouts);
-app.locals.title = 'IronDrones';
+app.locals.title = 'ToFlamaDrones';
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -13,12 +13,25 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+
+router.get('/new', (req, res, next) => {
+  res.render('drones/new');
 });
 
-router.post('/drones', (req, res, next) => {
-  // Iteration #3
+router.post('/', (req, res, next) => {
+  const droneInfo = {
+      droneName: req.body.name,
+      propellers: req.body.propellers,
+      maxSpeed: req.body.maxspeed,
+  };
+
+  const newDrone = new Drone(droneInfo);
+
+  newDrone.save( (err) => {
+    if (err) { return next(err) }
+    // redirect to the list of products if it saves
+    return res.redirect('/drones');
+  });
 });
 
 module.exports = router;
