509ab3b626bf60707437187c9d133159c4d4b502
Manuel Ortega <coolhunting@MacBook-Pro-de-Manuel.local>
2017-11-09 13:34:47 +0100

    Iteracion 1 Completa

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,31 @@
-// Iteration #1
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev', {useMongoClient: true});
+const Dron = require('../models/drone');
+
+
+const drone = [
+  {
+    droneName : "Manu",
+    propeller : 4,
+    maxSpeed: 100,
+  },
+
+  {
+    droneName : "Gema",
+    propeller : 2,
+    maxSpeed: 120,
+  },
+];
+
+//Dron.collection.drop();
+
+Dron.create(drone, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+
+  docs.forEach((dron) => {
+    console.log(dron.droneName)
+  });
+  mongoose.connection.close();
+});
