3c959177cb0975f155f1e176e6cc1bc987b40db1
Ernesmh <ernesmh@gmail.com>
2017-11-09 13:59:35 +0100

    close to the end

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -15,12 +15,24 @@ router.get('/', (req, res, next) => {
   });
 
 
-router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+router.get('/new', (req, res, next) => {
+    res.render('drones/new');
 });
 
 router.post('/drones', (req, res, next) => {
-  // Iteration #3
+  const dronecito = {
+      droneName: req.body.droneName,
+      propellers: req.body.propellers,
+      maxSpeed: req.body.speed,
+  };
+
+  // const newProduct = new Product(productInfo);
+  //   // Save the product to the DB
+  //    newProduct.save( (err) => {
+  //      if (err) { return next(err) ;}
+  //      // redirect to the list of products if it saves
+  //      return res.redirect('/products');
+  //    });
 });
 
 module.exports = router;
