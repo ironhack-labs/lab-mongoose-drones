97dd1ca7480a53e96be15bed0d841a80c8d51f62
Eloi Gil <eloigil6@gmail.com>
2017-11-11 20:34:24 +0100

    pull request

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,22 @@
 // Iteration #1
+'use strict';
+
+const mongoose = require("mongoose");
+const Drone = require('../models/Drone').Drone;
+
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const droneData = [
+    { droneName: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { droneName: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { droneName: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+Drone.create(droneData, function(err, result) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('YEEEAH! We feed our DBS');
+    }
+
+});
