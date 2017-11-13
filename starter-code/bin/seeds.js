ec0a41b9b56bc5482dc5dfbdcaf7c7b5233561a9
Jessica Alin Gonzalez <jessicaalin@Jessicas-MacBook-Pro.local>
2017-11-13 18:35:18 -0500

    iteration 1 done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,31 @@
 // Iteration #1
+require("../config/mongoose-setup");
+
+const DroneModel = require("../models/drone-model");
+
+const droneInfo = [
+  {
+    droneName: "Pterodactyl",
+    propellers: 8,
+    maxSpeed: 240
+  },
+  {
+    droneName: "Sordes",
+    propellers: 6,
+    maxSpeed: 200
+  },
+  {
+    droneName: "Jeholopterus",
+    propellers: 4,
+    maxSpeed: 160
+  }
+];
+
+DroneModel.create(droneInfo)
+  .then((droneResults) => {
+    console.log('Inserted ${droneResults.length} drones');
+  })
+  .catch((err) => {
+    console.log("Drone insert error!");
+    console.log(err);
+  });
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,12 +1,24 @@
 const express = require('express');
 
 // require the Drone model here
+require("../models/drone-model");
 
 const router = express.Router();
 
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+  DroneModel
+  .find()
+  .exec()
+  .then((droneResults) => {
+    // create a local variable for the view to access the DB results
+    res.locals.listOfDrones = droneResults;
+    res.render("drones/index");
+  })
+  .catch((err) => {
+    next(err);
+  });
 });
 
 
