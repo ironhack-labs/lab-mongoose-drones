136fa1ecfc69e0d07fc51a73e56439c7479c9dad
David Cohen <davidmartincohen@gmail.com>
2018-01-06 10:03:22 +0100

    done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,35 @@
 // Iteration #1
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const drones = [
+    {
+        name: "Military Drone",
+        propellers: 4,
+        maxSpeed: 25
+    },
+    {
+        name: "Family Drone",
+        propellers: 4,
+        maxSpeed: 10
+    },
+    {
+        name: "Racing Drone",
+        propellers: 8,
+        maxSpeed: 50
+    }
+]
+
+let Drone = require("../models/drone");
+
+Drone.create(drones, (err, docs) => {
+    if (err) {
+        console.log("ERROR", err);
+    }
+    else {
+        console.log("SUCCESS", docs);
+    }
+    mongoose.connection.close();
+
+});
