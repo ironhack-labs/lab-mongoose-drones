5a621e554cebf0eed78276f185723c16b808e512
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-01-13 15:55:53 +0100

    Feat: Iteration 2 done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,20 +1,25 @@
 const express = require('express');
 
 // require the Drone model here
+const Drone= require("../models/drone.model.js")
 
 const router = express.Router();
 
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2
+router.get('/', (req, res, next) => {
+  Drone.find({}).then((drones) => {
+    res.render('drones/index', {
+      drones: drones
+    });
+  });
 });
 
 
-router.get('/drones/new', (req, res, next) => {
+router.get('/new', (req, res, next) => {
   // Iteration #3
 });
 
-router.post('/drones', (req, res, next) => {
+router.post('/', (req, res, next) => {
   // Iteration #3
 });
 
