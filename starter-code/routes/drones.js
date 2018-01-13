e98380e14d08e46fec2733f32e7767988b7476a4
mouyaq <mouyaq@gmail.com>
2018-01-13 15:48:04 +0100

    feat: Iteration 2 done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,24 @@
 const express = require('express');
+const router = express.Router();
+const dronesController = require('../controllers/drones.controller');
 
 // require the Drone model here
 
-const router = express.Router();
+
+router.get('/', dronesController.index);
 
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2
-});
+// router.get('/drones', (req, res, next) => {
+//   // Iteration #2
+// });
 
 
-router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
-});
+// router.get('/drones/new', (req, res, next) => {
+//   // Iteration #3
+// });
 
-router.post('/drones', (req, res, next) => {
-  // Iteration #3
-});
+// router.post('/drones', (req, res, next) => {
+//   // Iteration #3
+// });
 
 module.exports = router;
