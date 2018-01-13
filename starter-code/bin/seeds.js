0afd36eedbd72f34ebb360acf8c1cb718e3d18b6
Jorge Yago <jorge.yago.meniz@gmail.com>
2018-01-13 16:07:22 +0100

    Iteration 1

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,30 @@
-// Iteration #1
+const mongoose = require('mongoose');
+
+require ('../config/db.config');
+
+const Drone = require ('../models/drone');
+
+const drones = [
+  {
+    name: "Phantom 1" ,
+    propellers: 4 ,
+    maxSpeed: 50,
+  },
+  {
+    name: "Phantom 2" ,
+    propellers: 6,
+    maxSpeed: 70,
+  },
+  {
+    name: "Phantom 3" ,
+    propellers: 8,
+    maxSpeed: 100,
+  }
+];
+
+Drone.create(drones).then((data) => {
+    data.forEach((element) => {
+      console.log(element.name);
+    });
+    mongoose.connection.close(); //duda qué hace
+});
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-// require the Drone model here
+const Drone = require('../models/drone');
 
 const router = express.Router();
 
