720210896903123cd4210e81d0d06c9ecfcf239e
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-01-15 11:41:00 +0100

    Feat: DELETE

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -17,26 +17,46 @@ router.get('/', (req, res, next) => {
 
 router.get('/new', (req, res, next) => {
   // Iteration #3
-  res.render('drones/new')
+  res.render('drones/new', {drone: new Drone()})
 });
 
 router.post('/', (req, res, next) => {
   // Iteration #3
   const droneData = req.body;
-
   const newDrone = new Drone(droneData);
-
   newDrone.save().then((drone) => {
-    //res.send("hola");
     res.redirect('/drones');
   })
 });
 
 router.get('/edit/:id', (req, res, next) => {
-  // Iteration #3  
-  let id = req.params.id;
-  res.send(id);
-  // res.render('drones/ ')
+  Drone.findById(req.params.id).then((drone) => {
+    res.render('drones/new', {
+      drone: drone
+    });
+  });
+});
+
+router.post('/:id', (req, res, next) => {
+  // Iteration #3
+  const droneId = req.params.id;
+  const updates = {
+      name: req.body.name,
+      propellers: req.body.propellers,
+      maxSpeed: req.body.maxSpeed
+  };
+  Drone.findByIdAndUpdate(droneId, updates).then((drone) => {
+    res.redirect('/drones');
+  });
+});
+
+router.post('/delete/:id', (req, res, next) => {
+
+  const droneId = req.params.id;
+
+  Drone.findByIdAndRemove(droneId).then((drone) => {
+    return res.redirect('/drones');
+  });
 });
 
 module.exports = router;
