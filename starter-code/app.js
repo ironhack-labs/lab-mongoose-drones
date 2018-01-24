0c67b144812dc73501336ee22de7e82f13d0ed3f
Manu Pacheco <mpachecopal@gmail.com>
2018-01-24 17:09:45 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,14 +1,14 @@
-const express        = require('express');
-const path           = require('path');
-const favicon        = require('serve-favicon');
-const logger         = require('morgan');
-const cookieParser   = require('cookie-parser');
-const bodyParser     = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
-const mongoose       = require('mongoose');
-const index          = require('./routes/index');
-const users          = require('./routes/users');
-const drones         = require('./routes/drones');
+const mongoose = require('mongoose');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const drones = require('./routes/drones');
 
 mongoose.connect('mongodb://localhost/drones-dev');
 
@@ -25,7 +25,9 @@ app.locals.title = 'Drnz';
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -33,14 +35,14 @@ app.use('/', index);
 app.use('/drones', drones);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const Drone = require('../models/drone');
+
+const droneData = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+Drone.create(droneData, (err, docs) => {
+  if (err) {
+    console.log('err: ', err);
+  } else {
+    docs.forEach((doc, index) => {
+      console.log(`doc ${index} inserted ${doc.name}`);
+    });
+  }
+});
