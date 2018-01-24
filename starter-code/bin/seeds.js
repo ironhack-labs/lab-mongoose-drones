4a03f3616a4ec1269dd9f71d1d4280c02c5733f3
AlexBHdez <abhbretones@gmail.com>
2018-01-24 19:44:46 +0100

    done with bonus

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,49 @@
-// Iteration #1
+const mongoose = require('mongoose');
+const { url, dbName } = require('../config');
+
+mongoose.connect(`mongodb://${url}/${dbName}`);
+
+const Drone = require('../models/drones');
+
+const drones = [
+  {
+    name: 'Marx',
+    propellers: 4,
+    maxSpeed: 60,
+  },
+  {
+    name: 'Olimpia',
+    propellers: 3,
+    maxSpeed: 40,
+  },
+  {
+    name: 'Kira',
+    propellers: 2,
+    maxSpeed: 20,
+  },
+  {
+    name: 'Creeper XL 500',
+    propellers: 3,
+    maxSpeed: 12
+  },
+  {
+    name: 'Racer 57',
+    propellers: 4,
+    maxSpeed: 20
+  },
+  {
+    name: 'Courier 3000i',
+    propellers: 6,
+    maxSpeed: 18
+  }
+];
+
+Drone.create(drones, (err, docs) => {
+  if (err) {
+    console.log('ERROR = ', err);
+  } else {
+    docs.forEach((doc, index) => {
+      console.log(`doc ${doc} inserted at position ${index}`)
+    });
+  }
+});
