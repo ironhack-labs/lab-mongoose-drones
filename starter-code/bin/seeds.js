ee269ac2426e3c0c7fd574f45991a3ec75b08839
AlbertoSP <ALBERTOSANCHEZHTML@GMAIL.COM>
2018-02-01 18:58:44 +0100

    creado el archivo node.js

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,34 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.js');
+
+mongoose.connect('mongodb://localhost/drones-dev');
+
+
+const droneData = [{
+        name: 'Creeper XL 500',
+        propellers: 3,
+        maxSpeed: 12
+    },
+    {
+        name: 'Racer 57',
+        propellers: 4,
+        maxSpeed: 20
+    },
+    {
+        name: 'Courier 3000i',
+        propellers: 6,
+        maxSpeed: 18
+    }
+];
+Drone.collection.drop();
+
+droneData.forEach( drone => {
+    let newDrone =  new Drone(drone);
+    newDrone.save((err, newSavedDrone)=>{
+        if (err){
+            throw err;
+        }
+        console.log(`producto guardado ${newSavedDrone.name}`);
+    })
+});
