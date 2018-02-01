4b19cce225625064b8d935bd761b44cdf21e2927
Victor Pellerin <pellerin.victor@gmail.com>
2018-02-01 16:45:03 +0100

    second iteration

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -3,18 +3,27 @@ const express = require('express');
 // require the Drone model here
 
 const router = express.Router();
+const Drone = require('../models/drone');
 
 
-router.get('/drones', (req, res, next) => {
+router.get('/', (req, res, next) => {
   // Iteration #2
+  
+  Drone.find({},(err, droneArray) => {
+    if (err) {
+      return next(err);
+    } else {
+      let droneObject = {arrayOfDrones: droneArray};
+      res.render('drones/index', droneObject);
+    }
+  });
 });
 
-
-router.get('/drones/new', (req, res, next) => {
+router.get('/new', (req, res, next) => {
   // Iteration #3
 });
 
-router.post('/drones', (req, res, next) => {
+router.post('/', (req, res, next) => {
   // Iteration #3
 });
 
