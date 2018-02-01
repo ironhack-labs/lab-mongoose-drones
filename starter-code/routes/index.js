b1905f2dc3ed5309fe8327cea4836279ff9b1e6a
Ana Recuero <anarecsa@gmail.com>
2018-02-01 16:11:09 +0100

    First drones created

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Drone = require("../models/drone.js");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
