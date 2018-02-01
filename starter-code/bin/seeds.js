86b71e2c77b81d44e7516dea8f495037f2fb28c5
Adrián Chacón <adrianchaconmadueno@gmail.com>
2018-02-01 16:01:00 +0100

    Iteración 1

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,39 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone');
+
+mongoose.connect('mongodb://localhost/drones-dev').then(() => console.log("Conectado con Seeds!"));
+
+const drones = [
+    {
+        name : "Millenium Falcon",
+        propellers : 2,
+        maxSpeed : 20,
+    },
+    {
+        name : "Air Force One",
+        propellers : 4,
+        maxSpeed : 25,
+    },
+    {
+        name : "Larry Bird",
+        propellers : 8,
+        maxSpeed : 10,
+    }
+];
+
+Drone.collection.drop();
+
+drones.forEach( p => {
+    let dr = new Drone(p);
+    dr.save((err,dron) =>{
+        if (err){
+            throw err;
+        }
+        console.log (`Dron guardado ${dron.name}`)
+    })
+});
+
+setTimeout( function () {
+    mongoose.disconnect();
+  }, 1000);
