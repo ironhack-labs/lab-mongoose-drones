2ea30441215a93673b3852ce5806593646d2dd1b
Adrián Chacón <adrianchaconmadueno@gmail.com>
2018-02-01 16:47:53 +0100

    Finalizado, no hay bonus ;)

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -14,12 +14,18 @@ router.get('/', (req, res, next) => {
 });
 
 
-router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+router.get('/new', (req, res, next) => {
+  res.render('drones/new');
 });
 
-router.post('/drones', (req, res, next) => {
+router.post('/new', (req, res, next) => {
   // Iteration #3
+  const {name,propellers,maxSpeed} = req.body;
+  const drone = new Drone({name,propellers,maxSpeed});
+  drone.save( err => {
+    if (err) { return next(err) }
+    res.redirect('/drones');
+  })
 });
 
 module.exports = router;
