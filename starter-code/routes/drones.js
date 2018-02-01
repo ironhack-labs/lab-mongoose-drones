3eec47960b03b7c5221ebdb0c1116c5fec6b683e
Pere Amorós <amorospere@gmail.com>
2018-02-01 15:24:41 +0100

    Iteration 2

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,20 +1,37 @@
 const express = require('express');
 
 // require the Drone model here
+const Drone = require('../models/drone');
 
 const router = express.Router();
 
 
-router.get('/drones', (req, res, next) => {
+router.get('/', (req, res, next) => {
   // Iteration #2
+  Drone.find({}, (err, result) => {
+    if(err){
+      next();
+    }
+    else{
+      let drones = {
+        droneName : result.name,
+        droneProp : result.propeller,
+        droneMaxSpeed : result.maxSpeed,
+        drones : result
+      }
+
+      res.render('drones/index', drones)
+    }
+
+  })
 });
 
 
-router.get('/drones/new', (req, res, next) => {
+router.get('/new', (req, res, next) => {
   // Iteration #3
 });
 
-router.post('/drones', (req, res, next) => {
+router.post('/', (req, res, next) => {
   // Iteration #3
 });
 
