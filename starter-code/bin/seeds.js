e571d78bb6fcbaf92f3b813f831cb4cd96568a24
Josh Vardey <jocelynvardey@gmail.com>
2018-02-01 15:12:28 +0100

    PAR Hanna Josh

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,32 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/drones-dev");
+const Drone = require("../models/drone");
+
 // Iteration #1
+const drones = [
+  {
+    name: "name1",
+    propellers: 5,
+    maxSpeed: 0
+  },
+  {
+    name: "name2",
+    propellers: 0,
+    maxSpeed: 5
+  },
+  {
+    name: "name3",
+    propellers: 6,
+    maxSpeed: 3
+  }
+];
+
+Drone.create(drones, (err, docs) => {
+  if (err) {
+    throw err;
+  }
+  docs.forEach(drone => {
+    console.log(drone.name);
+  });
+  mongoose.connection.close();
+});
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,37 @@
-const express = require('express');
-
+const express = require("express");
+const droneModel = require("../models/drone");
 // require the Drone model here
 
 const router = express.Router();
 
-
-router.get('/drones', (req, res, next) => {
-  // Iteration #2
+router.get("/", (req, res, next) => {
+  droneModel.find({}, (err, drones) => {
+    if (err) {
+      return next(err);
+    }
+    res.render("drones/index", { drones: drones });
+  });
 });
 
-
-router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
+router.get("/new", (req, res, next) => {
+  res.render("drones/new");
 });
 
-router.post('/drones', (req, res, next) => {
-  // Iteration #3
+router.post("/", (req, res, next) => {
+  const droneInfo = {
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  };
+
+  const newDrone = new droneModel(droneInfo);
+
+  newDrone.save(err => {
+    if (err) {
+      return next(err);
+    }
+    return res.redirect("/drones");
+  });
 });
 
 module.exports = router;
