ce4cb9a0948720cbd1fb9210aaf7bfa8a21bab24
Josep Salvat <jsalvat87@gmail.com>
2018-02-01 15:22:57 +0100

    LastFirstCommit

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,39 @@
 // Iteration #1
+const mongoose = require('mongoose');
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/drones-dev', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+const Drone = require('../models/drone');
+
+const drones = [
+    {
+        name       : 'NSA Surveillance Drone',
+        propellers      : 6,
+        maxSpeed   : 10,
+    },
+    {
+        name       : 'DJI Mavic Pro',
+        propellers      : 8,
+        maxSpeed   : 15,
+    },
+    {
+        name       : 'DJI Phantom 3',
+        propellers      : 10,
+        maxSpeed   : 20,
+    }
+  ];
+
+  Drone.create(drones, (err, data) => {
+    if (err) {
+      throw err;
+    }
+  
+    data.forEach((drones) => {
+      console.log(drones.name)
+    });
+    mongoose.connection.close();
+  });
