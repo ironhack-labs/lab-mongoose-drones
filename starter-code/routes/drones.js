83aca8e3a39230388c2d04c6cb0d10055fe51229
davidalexteixeira <david5teixeira@gmail.com>
2018-02-01 15:20:05 +0100

    second commit

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,20 +1,35 @@
+'use strict';
+
 const express = require('express');
 
 // require the Drone model here
 
 const router = express.Router();
 
+const Drone = require('../models/drone');
 
-router.get('/drones', (req, res, next) => {
+router.get('/', (req, res, next) => {
   // Iteration #2
+
+  Drone.find({}, (err, drones) => {
+    if (err) {
+      return next(err);
+    }
+    res.render('./drones/index', {
+      drones: drones
+    });
+  });
+  //   drones.forEach((drones) => {
+  //     console.log(' --> drones: ', drones.name);
+  //   });
+  // });
 });
 
-
-router.get('/drones/new', (req, res, next) => {
+router.get('/new', (req, res, next) => {
   // Iteration #3
 });
 
-router.post('/drones', (req, res, next) => {
+router.post('/', (req, res, next) => {
   // Iteration #3
 });
 
