2d35fbebd646bef5eecd3c5db1e5f658b8cd3af7
JoniWanKenobi <joniwankenoby@gmail.com>
2018-02-04 18:26:12 +0100

    Iteration 2 ok...

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,34 @@
 const express = require('express');
+const Drone = require('../models/drone');
 
 // require the Drone model here
 
 const router = express.Router();
 
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2
+router.get('/', (req, res, next) => {
+  // Iteration #2  
+  Drone.find({}, (err, drones)=>{
+    if(err){
+      console.log('Error fetching from DB');
+      return next(err);
+    } else {
+      console.log(drones.length);
+      const data = {
+        drones : drones
+      };
+      res.render('drones/index', data);
+    }
+  });
 });
 
 
-router.get('/drones/new', (req, res, next) => {
-  // Iteration #3
-});
+// router.get('/drones/new', (req, res, next) => {
+//   // Iteration #3
+// });
 
-router.post('/drones', (req, res, next) => {
-  // Iteration #3
-});
+// router.post('/drones', (req, res, next) => {
+//   // Iteration #3
+// });
 
 module.exports = router;
