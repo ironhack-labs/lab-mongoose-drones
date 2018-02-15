9d6fef4a054f3d58748aed32dc5ee1634ec0b6c1
Lance Meier <lancemeier@gmail.com>
2018-02-15 13:50:13 -0600

    projecto completatdo

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,18 @@
 // Iteration #1
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const Drone = require("../models/Drone.js");
+
+const droneData = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+Drone.create(droneData, (err,result) => {
+  console.log(result);
+  mongoose.connection.close();
+});
+
