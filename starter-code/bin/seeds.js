e31d2d83ccf3ab9f4b5d4d5335b27a143df95717
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-02-15 13:09:52 -0600

    Iteration 2

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,20 @@
+const mongoose = require ("mongoose");
+mongoose.connect("mongodb://localhost/drones-dev");
+
 // Iteration #1
+
+const Drone = require("../models/Drone.js");
+const droneData = [
+  { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+  { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+  { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+];
+
+Drone.collection.drop();
+
+Drone.create(droneData, (err, result) =>{
+  console.log(result);
+  mongoose.connection.close();
+
+});
+
--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
+const Drone = require("../models/Drone.js")
 // require the Drone model here
 
 const router = express.Router();
 
 
-router.get('/drones', (req, res, next) => {
-  // Iteration #2
+router.get('/', (req, res, next) => {
+  console.log('holaaa')
+  res.render('drones/index')
 });
 
 
