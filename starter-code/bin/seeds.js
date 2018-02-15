37d32c5cf13415acbce2f821a7ab13ef744c41db
Ori Bermudez <ori.bermudez@outlook.com>
2018-02-15 13:55:56 -0600

    Done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,21 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const Drone = require("../models/Drone.js");
+
+const droneData = [
+    { name: 'Creeper XL 500', propellers: 3, maxSpeed: 12 },
+    { name: 'Racer 57', propellers: 4, maxSpeed: 20 },
+    { name: 'Courier 3000i', propellers: 6, maxSpeed: 18 }
+  ];
+
+//Limpia la colección de drones
+Drone.collection.drop();
+
+Drone.create(droneData, (err,result) => {
+    result.forEach((drone) => console.log(`El drone ${drone} ha sido agregado exitosamente`));
+    mongoose.connection.close();
+});
+
