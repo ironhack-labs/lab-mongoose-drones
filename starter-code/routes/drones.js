e7a0f6e7a666d0df5f6b546892bb4e5f39be494b
Debby ermel <debbyermel@gmail.com>
2018-02-15 13:38:00 -0600

    Iteration 2 Done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
 // require the Drone model here
-
+const Drone = require("../models/Drone.js");
 const router = express.Router();
 
-
-router.get('/drones', (req, res, next) => {
   // Iteration #2
+router.get('/', (req, res, next) => {
+  Drone.find({},(err,result)=> {
+    console.log(result);
+    res.render("drones/index", {result})
+   
+  })
+  //next();
 });
 
 
