b6522f0d2a53eac9ed16925040edb0d4da9e069f
Pablo Gomez <gomezpa@bellsouth.net>
2018-02-15 17:07:30 -0500

    done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,33 @@
 // Iteration #1
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/drones-dev');
+
+const Drone = require("../models/drone")
+
+const drones = [
+    {
+      name: "Silver Surfer",
+      propellers: 4,
+      maxSpeed: 100,
+    },
+    {
+        name: "The Tittler",
+        propellers: 8,
+        maxSpeed: 120,
+    },
+    {
+        name: "Hodor",
+        propellers: 12,
+        maxSpeed: 140,
+    }
+  ];
+
+  Drone.create(drones, (err, docs) => {
+    if (err) {
+        throw err;
+    }
+    docs.forEach((drone) => {
+      console.log(drone.name);
+    });
+    mongoose.connection.close();
+});
