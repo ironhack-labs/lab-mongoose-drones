6ae0bf06e55bd2aac80514184375b88c5379ebc6
Diego Arroyo <arroyodiego1@gmail.com>
2018-02-15 15:34:10 -0600

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -32,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/drones', drones);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
---
+++ starter-code/models/Drone.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const droneSchema = new Schema({
+  name:String,
+  propellers:Number,
+  maxSpeed:Number,
+});
+
+module.exports = mongoose.model("Drone", droneSchema);
