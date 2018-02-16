32ae2ad629ddac9e7e15bdd19e6c990e04b7d4b6
Rakeem <thomas.rakeem.29@gmail.com>
2018-02-16 08:28:55 -0500

    initial

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -23,6 +23,32 @@ router.post('/', (req, res, next) => {
   res.redirect('/drones');
 });
 
+router.get('/edit', (req, res) => res.render('drones/edit'));
+
+router.get('/:id/edit', (req, res, next) => {
+  const id = req.params.id;
+  Drone.findById(id, (err, drone) => {
+    if (err) throw next(err);
+    res.render('drones/edit', { drone });
+  });
+});
+
+router.post('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  Drone.findById(id, (err, drone) => {
+    if (err) throw next(err);
+    const updatedDroneData = {
+      name: req.body.name,
+      propellers: req.body.propeller,
+      maxSpeed: req.body.maxSpeed,
+    };
+    Drone.findByIdAndUpdate(id, updatedDroneData, (err, drone) => {
+      if (err) throw next(err);
+      return res.redirect('/drones');
+    });
+  });
+});
 router.get('/new', (req, res, next) => {
   res.render('drones/new');
 });
