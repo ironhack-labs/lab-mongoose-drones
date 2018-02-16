78e24d3c341bbbe9fc94545e30edac667d95ef53
nlevo <nikola.levo@gmail.com>
2018-02-15 22:01:52 -0500

    Done

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -1,21 +1,47 @@
 const express = require('express');
 
 // require the Drone model here
+const Drone = require('../models/drone');
 
 const router = express.Router();
 
 
-router.get('/drones', (req, res, next) => {
+router.get('/', (req, res, next) => {
   // Iteration #2
+    Drone.find({}, (err, drones) => {
+        if(err) 
+            return next(err)
+        res.render('drones/index', {
+            drones: drones
+        });
+    });
+  });
+
+
+router.get('/new', (req, res, next) => {
+  // Iteration #3
+  res.render('drones/new');
 });
 
-
-router.get('/drones/new', (req, res, next) => {
+router.post('/', (req, res, next) => {
   // Iteration #3
-});
+  //take the params and translate them into a new object
+  const droneInfo = {
+    name:       req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed:   req.body.maxSpeed,
+}
+//create a new product with the params passed in 
+//form the "/products/new" form
+const newDrone = new Drone(droneInfo);
 
-router.post('/drones', (req, res, next) => {
-  // Iteration #3
+newDrone.save((err) => {
+    if (err) {
+        return next(err);
+    }
+    //redirect to the list of products (/products) if it saves.
+    return res.redirect('/drones');
+  });
 });
 
 module.exports = router;
