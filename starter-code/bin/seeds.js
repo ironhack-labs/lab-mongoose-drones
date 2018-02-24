1da7180336b22b6fd1313b75b2d4fa554101e4e1
Maxim <maximlucov@yahoo.com>
2018-02-24 16:31:56 +0100

    Done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,1 +1,12 @@
 // Iteration #1
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/drones-dev", { useMongoClient: true });
+const Drone = require("../models/drone");
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+Drone.create(drones, () => {
+  mongoose.connection.close();
+});
